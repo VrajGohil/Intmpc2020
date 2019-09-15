8383e5160d718606ac40c99f4c9453705ab54e23
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-09-15 11:33:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFCA437A),
+          title: Center(
+            child: Text(
+              "INTMPC 2020",
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        body: HelloWorld(),
+      ),
+    ),
+  );
+}
+
+class HelloWorld extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+        colors: [const Color(0xFF915FB5), const Color(0xFFCA436B)],
+      )),
+      child: Center(
+        child: Text(
+          'Webite Under development.\nStay tuned!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+  /*@override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Text(
+            'Webite Under development.\nStay tuned!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }*/
+}
