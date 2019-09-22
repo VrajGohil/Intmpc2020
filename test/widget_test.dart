adbee206f39768b7b2fc2e805702d78e37151ebe
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-09-22 10:21:20 +0530

    Trying Flare for 2D Animations

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,16 +1,16 @@
-//// This is a basic Flutter widget test.
-////
-//// To perform an interaction with a widget in your test, use the WidgetTester
-//// utility that Flutter provides. For example, you can send tap and scroll
-//// gestures. You can also use WidgetTester to find child widgets in the widget
-//// tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
 //
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//
-//import 'package:intmpc/main.dart';
-//
-//void main() {
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:intmpc/main.dart';
+
+void main() {
 //  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //    // Build our app and trigger a frame.
 //    await tester.pumpWidget(HelloWorld());
@@ -27,4 +27,4 @@
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
 //  });
-//}
+}
