c28cc87bc4823b2de36dfa3a68a7b0f71a637d58
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-10-06 15:33:20 +0530

    Added Basic Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color(0xFFCA437A),
-          title: Center(
-            child: Text(
-              "INTMPC 2020",
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        body: HelloWorld(),
-      ),
-    ),
-  );
+  runApp(HomePage());
 }
 
-class HelloWorld extends StatelessWidget {
+class HomePage extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: new BoxDecoration(
-          gradient: new LinearGradient(
-        colors: [const Color(0xFF915FB5), const Color(0xFFCA436B)],
-      )),
-      child: Center(
-        child: Text(
-          'Website Under development.\nStay tuned!',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.white,
-          ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: PageView(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/whiteBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: FlatButton(
+                      onPressed: () {
+                        //TODO: Add menu here
+                      },
+                      child: Icon(
+                        Icons.menu,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Image.asset('assets/logo.png'),
+                        width: 600.0,
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      FloatingActionButton.extended(
+                        onPressed: () {
+                          //TODO: Add action here
+                        },
+                        backgroundColor: Colors.black,
+                        label: Text("Participate Now"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/blackBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
+          ],
+          scrollDirection: Axis.vertical,
         ),
       ),
     );
   }
-  /*@override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blueAccent,
-        child: Center(
-          child: Text(
-            'Website Under development.\nStay tuned!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }*/
 }
+
