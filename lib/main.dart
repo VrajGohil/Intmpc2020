a7e721aef915b189f2636fda88fe33b6550fde6f
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-10-07 12:48:02 +0530

    Added Basic Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,39 @@ void main() {
   runApp(HomePage());
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  bool isPlaying = false;
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    void _handleOnPressed() {
+      setState(() {
+        isPlaying = !isPlaying;
+        isPlaying
+            ? _animationController.forward()
+            : _animationController.reverse();
+      });
+    }
+
     return MaterialApp(
       theme: ThemeData(
         // Define the default brightness and colors.
@@ -29,17 +59,27 @@ class HomePage extends StatelessWidget {
                     fit: BoxFit.fill,
                   ),
                 ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_up,
+                        color: Colors.black,
+                      ),
+                      onPressed: () {}),
+                ),
                 SafeArea(
                   child: Align(
                     alignment: Alignment.topRight,
-                    child: FlatButton(
-                      onPressed: () {
-                        //TODO: Add menu here
-                      },
-                      child: Icon(
-                        Icons.menu,
+                    child: IconButton(
+                      iconSize: 50,
+                      icon: AnimatedIcon(
+                        icon: AnimatedIcons.menu_close,
                         color: Colors.black,
+                        size: 35.0,
+                        progress: _animationController,
                       ),
+                      onPressed: () => _handleOnPressed(),
                     ),
                   ),
                 ),
@@ -78,6 +118,162 @@ class HomePage extends StatelessWidget {
                     height: double.infinity,
                     width: double.infinity,
                     fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'How To Enter',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.white,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/whiteBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Awards',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.black,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/blackBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Host',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.white,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/whiteBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Jury',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.black,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/blackBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'INTMPC\n  2018',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.white,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: <Widget>[
+                Center(
+                  child: Image.asset(
+                    'assets/whiteBg.png',
+                    height: double.infinity,
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Contact',
+                        style: TextStyle(
+                          fontFamily: 'Brush',
+                          color: Colors.black,
+                          fontSize: 50.0,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
